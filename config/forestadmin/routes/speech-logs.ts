be1d312010e061ea9abf6424baf3e15f01605f4c
Christiaan Maks <christilut@gmail.com>
2017-11-07 15:21:41 +0100

    heroku debugging

--- config/forestadmin/routes/speech-logs.ts
+++ config/forestadmin/routes/speech-logs.ts
@@ -1,41 +1,41 @@
-// import * as Liana from 'forest-express-mongoose'
-// import AWS from 'config/aws'
-// import * as Moment from 'moment'
+import * as Liana from 'forest-express-mongoose'
+import AWS from 'config/aws'
+import * as Moment from 'moment'
 
-// import { router } from 'config/express'
+import { router } from 'config/express'
 
-// const logs = new AWS.CloudWatchLogs()
+const logs = new AWS.CloudWatchLogs()
 
-// router.get('/forest/speech', Liana.ensureAuthenticated, async function (req, res) {
-//   const result = await logs.getLogEvents({
-//     logGroupName: 'digital-tom',
-//     logStreamName: Moment().format('YYYY-MM-DD') // TODO
-//   }).promise()
+router.get('/forest/speech', Liana.ensureAuthenticated, async function (req, res) {
+  const result = await logs.getLogEvents({
+    logGroupName: 'digital-tom',
+    logStreamName: Moment().format('YYYY-MM-DD') // TODO
+  }).promise()
 
-//   const speech = result.events
-//     .filter(x => {
-//       const anyEvent = JSON.parse(x.message)
+  const speech = result.events
+    .filter(x => {
+      const anyEvent = JSON.parse(x.message)
 
-//       return anyEvent.meta.event === 'speech'
-//     })
-//     .map(logEvent => {
-//       const speechEvent = JSON.parse(logEvent.message)
+      return anyEvent.meta.event === 'speech'
+    })
+    .map(logEvent => {
+      const speechEvent = JSON.parse(logEvent.message)
 
-//       return {
-//         timestamp: logEvent.timestamp,
-//         message: speechEvent.meta.message
-//       }
-//     })
+      return {
+        timestamp: logEvent.timestamp,
+        message: speechEvent.meta.message
+      }
+    })
 
-//   res.send({
-//     data: speech.map(wordObject => {
-//       return {
-//         type: 'speech',
-//         attributes: {
-//           timestamp: new Date(wordObject.timestamp),
-//           words: wordObject.message
-//         }
-//       }
-//     })
-//   })
-// })
+  res.send({
+    data: speech.map(wordObject => {
+      return {
+        type: 'speech',
+        attributes: {
+          timestamp: new Date(wordObject.timestamp),
+          words: wordObject.message
+        }
+      }
+    })
+  })
+})
