af5748b84cab3fa59b8f9226f41dbdbcc2de3726
Christiaan Maks <christilut@gmail.com>
2017-11-07 14:12:26 +0100

    heroku debugging

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,5 @@ import 'config/logger'
 import 'config/mailgun'
 import 'config/sentry'
 import 'config/mongoose'
-import 'config/express'
+// import 'config/express'
 import 'config/aws'
-
-// module.parent check is required to support mocha watch
-// src: https://github.com/mochajs/mocha/issues/1912
-if (!module.parent) { // TODO check this with ava
-  // listen on port config.PORT
-
-}
